2f2a6ba8f8ed7a6b23e9fd1e3233bf5386c9205f
jovi <15909249610@163.com>
2025-01-12 21:59:55 +0800

    添加页面

---
+++ lib/pages/LoginPage..dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Loginpage extends StatefulWidget {
+  const Loginpage({super.key});
+
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/tools/netTool.dart
@@ -0,0 +1,13 @@
+// import 'dart:io';
+
+// Future<String> getNetworkData() async {
+//   await Future.delayed(Duration(seconds: 3));
+//   return "network data";
+// }
+
+// void main() async {
+//   print("main function start");
+//   var data = await getNetworkData();
+//   print(data);
+//   print("main function end");
+// }
